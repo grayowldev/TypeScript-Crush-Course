25c25b8eff90bca962b78f9ddef1af1e5ae7ff3c
grayowldev <truegrayowl@gmail.com>
2018-06-23 20:24:44 -0400

    Interface

--- main.ts
+++ main.ts
@@ -9,6 +9,7 @@ message = "abc";
 let endsWithC = (<string>message).endsWith("c" || "C");
 let altEndsWithC = (message as string).endsWith("c" || "C");
 
+
 //  Arrow Functions [Lamda]
 let log =  function(message){   //standard JS function variable
     console.log(message);
@@ -23,5 +24,27 @@ let lamdaLog2 = (message) => console.log(message);  //single line function algo
 let lamdaLog3 = message => {console.log(message);}  //single argument lamda function
 
 let lamdaLog4 = () => {console.log("no arguments/parameter")}   //no argument lamda function
+
+
+//  Custom TS Types
+let drawPoint = (a,b,c,d,e,f,g) => {
+    // try to avoid functions with many arguments
+}
+
+//--------------------------------------------------------------------------------------------------
+interface Point{    //use Pascal case/naming convension with interfaces
+    x: number,
+    y: number
+}
+
+let drawPoint2 = (point:Point) => {
+
+}
+    /*
+        Though the above interface/function relationship is valid, it violates the cohetion principle.
+        A better option is to use a class
+    */
+//--------------------------------------------------------------------------------------------------
+
 
 
