88d14aafe8837f947a090b46ad616e32cfc7810f
grayowldev <truegrayowl@gmail.com>
2018-06-23 20:10:18 -0400

    Arrow Functions [Lamda]

--- main.ts
+++ main.ts
@@ -3,11 +3,25 @@
 
 
 
-//Type Accerssions 
-let message2;
-message2 = "abc";
-let endsWithC = (<string>message2).endsWith("c" || "C");
-let altEndsWithC = (message2 as string).endsWith("c" || "C");
+//  Type Accerssions 
+let message;
+message = "abc";
+let endsWithC = (<string>message).endsWith("c" || "C");
+let altEndsWithC = (message as string).endsWith("c" || "C");
 
-//
+//  Arrow Functions [Lamda]
+let log =  function(message){   //standard JS function variable
+    console.log(message);
+}
+
+let lamdaLog = (message) => {  //standard and prefered arrow/lamda function
+    console.log(message);
+}
+
+let lamdaLog2 = (message) => console.log(message);  //single line function algo
+
+let lamdaLog3 = message => {console.log(message);}  //single argument lamda function
+
+let lamdaLog4 = () => {console.log("no arguments/parameter")}   //no argument lamda function
+
 
